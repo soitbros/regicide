9999f0587e7df9864c1f684176acfdf9044541fa
Clint Osterholz <clintosterholz@gmail.com>
2015-11-29 13:12:28 -0500

    adds working discard pile, previous adds was lies, all lies

--- scripts.js
+++ scripts.js
@@ -49,7 +49,6 @@ function cardStack() {
   this.makeDeck = cardDeck;
   this.shuffleDeck = cardShuffle;
   this.deckDeal = cardsDeal;
-  this.draw = cardDraw;
   this.addCard = addCards;
   this.combine = cardsCombine;
 }
@@ -59,13 +58,50 @@ function royalStack() {
   this.makeRoyals = royalDeck;
 }
 
-var deck = new cardStack();
-var hand = new cardStack();
-var discard = new cardStack();
-var royals = new royalStack();
+deck = new cardStack();
+hand = new cardStack();
+discard = new cardStack();
+royals = new royalStack();
 deck.makeDeck();
 deck.shuffleDeck();
 royals.makeRoyals();
+cardVolley();
+
+// inserts protagonists
+
+var playerKing = royals.cards[1];
+var playerQueen = royals.cards[0];
+
+$('.play_area .king').append(playerKing.createCard());
+$('.play_area .queen').append(playerQueen.createCard());
+$('.play_area .health_king').text(playerKing.cardHealth);
+$('.play_area .health_queen').text(playerQueen.cardHealth);
+
+// inserts opponents
+
+var opp1King = royals.cards[3];
+var opp1Queen = royals.cards[2];
+
+$('.opponent_one .king').append(opp1King.createCard());
+$('.opponent_one .queen').append(opp1Queen.createCard());
+$('.opponent_one .health_king').text(opp1King.cardHealth);
+$('.opponent_one .health_queen').text(opp1Queen.cardHealth);
+
+var opp2King = royals.cards[5];
+var opp2Queen = royals.cards[4];
+
+$('.opponent_two .king').append(opp2King.createCard());
+$('.opponent_two .queen').append(opp2Queen.createCard());
+$('.opponent_two .health_king').text(opp2King.cardHealth);
+$('.opponent_two .health_queen').text(opp2Queen.cardHealth);
+
+var opp3King = royals.cards[7];
+var opp3Queen = royals.cards[6];
+
+$('.opponent_three .king').append(opp3King.createCard());
+$('.opponent_three .queen').append(opp3Queen.createCard());
+$('.opponent_three .health_king').text(opp3King.cardHealth);
+$('.opponent_three .health_queen').text(opp3Queen.cardHealth);
 
 // standard deck creation
 
@@ -101,7 +137,6 @@ function cardShuffle() {
       this.cards[j] = this.cards[k];
       this.cards[k] = temp;
     }
-    cardVolley();
 }
 
 // combines cards
@@ -109,22 +144,6 @@ function cardShuffle() {
 function cardsCombine(stack) {
   this.cards = this.cards.concat(stack.cards)
   stack.cards = new Array();
-}
-
-// draws cards
-
-function cardDraw(n) {
-
-  var card;
-
-  if (n >= 0 && n < this.cards.length) {
-    card = this.cards[n];
-    this.cards.splice(n, 1);
-  }
-  else
-    card = null;
-
-  return card;
 }
 
 // creates cards
@@ -165,6 +184,38 @@ function makeCard() {
   return card;
 }
 
+// deals cards
+
+function cardsDeal() {
+
+  if (this.cards.length > 0)
+    return this.cards.shift();
+  else
+    return null;
+}
+
+function addCards(card) {
+  this.cards.push(card);
+}
+
+// event listeners
+
+$(".volley").on("click", deal);
+$(".tally").on("click", discards);
+
+
+function deal() {
+  if (deck === null) return;
+  if (deck.cards.length < 3)
+    alert("Not enough cards.");
+  else {
+    discards();
+    for (var i = 0; i < 3; i++)
+      hand.addCard(deck.deckDeal());
+  }
+  cardVolley();
+}
+
 // discards
 
 function discards() {
@@ -182,40 +233,14 @@ function reset() {
   cardVolley();
 }
 
-// deals cards
-
-function cardsDeal() {
-
-  if (this.cards.length > 0)
-    return this.cards.shift();
-  else
-    return null;
-}
-
-function addCards(card) {
-  this.cards.push(card);
-}
-
-function deal() {
-  if (deck === null) return;
-  if (deck.cards.length < 3)
-    alert("Not enough cards.");
-  else {
-    discards();
-    for (var i = 0; i < 3; i++)
-      hand.addCard(deck.deckDeal());
-  }
-  cardVolley();
-}
-
 // sets volley
 
 function cardVolley() {
   var left = 0;
   var top = 0;
   var starter = $(".deck");
-  // while (starter.firstChild !== null)
-  //   starter.remove(starter.firstChild);
+  while (starter.firstChild != null)
+    starter.removeChild(starter.firstChild);
   for (var i = 0; i < deck.cards.length; i++) {
     card = deck.cards[i].createCard();
     card.children().css("visibility", "hidden");
@@ -227,8 +252,8 @@ function cardVolley() {
   }
 
   var starter = $(".deal");
-  // while (starter.firstChild !== null)
-  //   starter.removeChild(starter.firstChild);
+  while (starter.firstChild != null)
+    starter.removeChild(starter.firstChild);
   for (var i = 0; i < hand.cards.length; i++) {
     card = hand.cards[i].createCard();
     card.css("margin-top", top + "em");
@@ -237,9 +262,9 @@ function cardVolley() {
   }
 
   var starter = $(".discard");
-  // while (starter.firstChild !== null)
-  //   starter.removeChild(starter.firstChild);
-  for (var i = discard.cards.length - 0; i < discard.cards.length; i++) {
+  while (starter.firstChild != null)
+    starter.removeChild(starter.firstChild);
+  for (var i = 0; i < discard.cards.length; i++) {
     card = discard.cards[i].createCard();
     card.css("margin-left", left + "em");
     card.css("margin-top", top + "em");
@@ -249,48 +274,15 @@ function cardVolley() {
   }
 }
 
-// inserts protagonists
 
-var playerKing = royals.cards[1];
-var playerQueen = royals.cards[0];
 
-$('.play_area .king').append(playerKing.createCard());
-$('.play_area .queen').append(playerQueen.createCard());
-$('.play_area .health_king').text(playerKing.cardHealth);
-$('.play_area .health_queen').text(playerQueen.cardHealth);
-
-// inserts opponents
-
-var opp1King = royals.cards[3];
-var opp1Queen = royals.cards[2];
-
-$('.opponent_one .king').append(opp1King.createCard());
-$('.opponent_one .queen').append(opp1Queen.createCard());
-$('.opponent_one .health_king').text(opp1King.cardHealth);
-$('.opponent_one .health_queen').text(opp1Queen.cardHealth);
-
-var opp2King = royals.cards[5];
-var opp2Queen = royals.cards[4];
-
-$('.opponent_two .king').append(opp2King.createCard());
-$('.opponent_two .queen').append(opp2Queen.createCard());
-$('.opponent_two .health_king').text(opp2King.cardHealth);
-$('.opponent_two .health_queen').text(opp2Queen.cardHealth);
-
-var opp3King = royals.cards[7];
-var opp3Queen = royals.cards[6];
-
-$('.opponent_three .king').append(opp3King.createCard());
-$('.opponent_three .queen').append(opp3Queen.createCard());
-$('.opponent_three .health_king').text(opp3King.cardHealth);
-$('.opponent_three .health_queen').text(opp3Queen.cardHealth);
-
-// cardVolley();
+// setTimeout(deal, 3500);
+// setTimeout(deal, 7000);
+// setTimeout(deal, 10000);
 
 // health bars
 
-$(".volley").on("click", deal());
-$(".tally").on("click", discards());
+
 
 // You will eventually also have a Discard Pile and a Graveyard.
 
@@ -322,6 +314,5 @@ $(".tally").on("click", discards());
 // }
 
 // win condition : last one standing
-
 
 }
